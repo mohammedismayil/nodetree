ef195285b15451f137fc8f2d90f4a52406e5764c
mohammedismayil <mohammedismayil10@gmail.com>
2021-05-16 13:53:59 +0530

    installed formidable package for file upload

--- first.js
+++ first.js
@@ -1,10 +1,13 @@
 var http = require('http');
-var dateobj = require('./dateModule')
+var dateobj = require('./dateModule');
+var url = require('url');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  res.write(req.url);
-
+  // var q = url.parse(req.url, true).query;
+  // var txt = q.year + " " + q.month;
+  // res.end(txt);
+ 
   res.write("The date and time are currently: " + dateobj.myDateTime());
   res.end('Hello World!');
 }).listen(8080);
