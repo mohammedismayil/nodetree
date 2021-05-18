b3a5c3a05fdebb98af2a081bb54a1d27c83f3140
mohammedismayil <mohammedismayil10@gmail.com>
2021-05-18 20:43:21 +0530

    added .env file

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8080
 app.get('/' , (req , res)=>{
 
    res.send('hello from simple server :)')
@@ -10,6 +14,6 @@ app.get('/' , (req , res)=>{
 //     res.send('hello from simple server - another route which was i created :)')
  
 //  })
-app.listen(3000,()=>{
-    console.log("Server is running on http://localhost:{30000}")
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
