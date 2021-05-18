ff16cf9135964aa62b7a1b56c7153884cb4e79e4
mohammedismayil <mohammedismayil10@gmail.com>
2021-05-18 21:18:19 +0530

    path has been set for ejs file

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+
+
 dotenv.config({path:'config.env'});
+
 const PORT = process.env.PORT || 8080
-app.get('/' , (req , res)=>{
 
-   res.send('hello from simple server :)')
 
-})
-// app.get('/testroute' , (req , res)=>{
+//for logging the request from the client to the server
+app.use(morgan('tiny'));
 
-//     res.send('hello from simple server - another route which was i created :)')
+app.use(bodyparser.urlencoded({extended:true}));
+
+// set view engine
+
+app.set("view engine","ejs");
+
+app.set('/views',express.static(path.resolve(__dirname,"views/index.ejs")))
+//for loading assets from the local project
+
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+
+
+// app.get('/' , (req , res)=>{
+
+//    res.send('hello from simple server :)')
+
+// })
+
+app.get('/' ,(req , res)=>{
+
+    res.render(`index`)
  
-//  })
+ })
+app.get('/testroute' , (req , res)=>{
+
+    res.send('hello from simple server - another route which was i created :)')
+ 
+ })
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
