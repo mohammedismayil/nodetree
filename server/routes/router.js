20e338c336f1c043256d14980d48a8e063c2ffe1
mohammedismayil <mohammedismayil10@gmail.com>
2021-05-25 23:02:19 +0530

    installed jwt

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const route = express.Router()
-
+const jwt = require('jsonwebtoken');
 const services = require('../services/render');
 const controller = require('../controller/controller');
+const dotenv = require('dotenv');
 
+// get config vars
+dotenv.config( { path : 'config.env'} )
+
+// access config var
+process.env.TOKEN_SECRET;
 /**
  *  @description Root Route
  *  @method GET /
@@ -30,4 +36,17 @@ route.put('/api/users/:id', controller.update);
 route.delete('/api/users/:id', controller.delete);
 
 route.post('/api/authuser', controller.authUserCreate);
+route.get('/me',authenticateToken,controller.getIn);
+function authenticateToken(req, res, next) {
+    // const authHeader = req.headers['authorization']
+    // const token = authHeader && authHeader.split(' ')[1]
+  const token = req.headers['token']
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        
+        res.status(200).send(decoded);
+      });
+  }
 module.exports = route
